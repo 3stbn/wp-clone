04e1e4d636d756d60e81dead15c09389e315b975
3stbn <esteban93torres@gmail.com>
2021-10-31 21:11:56 -0500

    checkpoint

--- firebase.js
+++ firebase.js
@@ -7,12 +7,7 @@ import {
 import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyD40BM2dG_dBr5s2Z4toSiwglDP77gL5qg",
-  authDomain: "wp-clone-aa3e3.firebaseapp.com",
-  projectId: "wp-clone-aa3e3",
-  storageBucket: "wp-clone-aa3e3.appspot.com",
-  messagingSenderId: "1072208407815",
-  appId: "1:1072208407815:web:80a0d0aa9f0f0c1e82e9a2",
+ // your config goes here
 };
 
 export const app = initializeApp(firebaseConfig);
